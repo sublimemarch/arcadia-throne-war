f07727817662b7bba15446df47aa34c18f23c85c
Korey Enright <korey.enright@oracle.com>
2015-04-19 17:24:48 -0500

    Generate models and migrations - complete models and migrations for User

---
+++ db/migrate/20150419220603_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :email, unique: true, null: false
+      t.string :name, null: false, unique: true
+      t.string :password_hash, null: false
+
+      t.int :type, null: false
+
+      t.timestamps null: false
+    end
+  end
+end
