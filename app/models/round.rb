bb3e0ca67e894f728317dc43fc6166b4bb07e7c8
Korey Enright <korey.enright@oracle.com>
2015-04-19 17:41:16 -0500

    Create model and migration for round

--- app/models/round.rb
+++ app/models/round.rb
@@ -1,2 +1,4 @@
 class Round < ActiveRecord::Base
+      has_many :pledges      
+
 end
--- db/migrate/20150419220725_create_rounds.rb
+++ db/migrate/20150419220725_create_rounds.rb
@@ -1,6 +1,9 @@
 class CreateRounds < ActiveRecord::Migration
   def change
     create_table :rounds do |t|
+      belongs_to :auction
+      
+      t.integer :number, default: 0
 
       t.timestamps null: false
     end
