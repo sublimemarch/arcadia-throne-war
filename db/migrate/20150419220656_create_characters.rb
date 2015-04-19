af2c1d01bdda7c1e6831cf8a4106a67b76d7408b
Red Lhota <rose.as.romeo@gmail.com>
2015-04-19 18:25:53 -0500

    Fix error in character migration.

--- db/migrate/20150419220656_create_characters.rb
+++ db/migrate/20150419220656_create_characters.rb
@@ -1,12 +1,12 @@
 class CreateCharacters < ActiveRecord::Migration
   def change
     create_table :characters do |t|
-      t.string :pseudonym, unique :true, null: false
+      t.string :pseudonym, unique: true, null: false
 
       t.belongs_to :user
       t.belongs_to :game
 
-      t.int :points_spent, default: 0, null: false
+      t.integer :points_spent, default: 0, null: false
 
       t.timestamps null: false
     end
