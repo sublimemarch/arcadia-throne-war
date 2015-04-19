67db73b33a691a1976c2eb8bad19e6f9e270d7a0
Stephanie Marx <stephanie.n.marx@gmail.com>
2015-04-19 17:53:21 -0500

    add model and migration for pledge

--- db/migrate/20150419220742_create_pledges.rb
+++ db/migrate/20150419220742_create_pledges.rb
@@ -1,6 +1,11 @@
 class CreatePledges < ActiveRecord::Migration
   def change
     create_table :pledges do |t|
+    	t.integer :rank, default: 0, null: false
+    	t.belongs_to :character, null: false
+    	t.belongs_to :round, null: false
+    	t.belongs_to :item, null: false
+    	t.integer :value, null: false
 
       t.timestamps null: false
     end
