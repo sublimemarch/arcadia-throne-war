e72e270772137e0c29fed68781745b6935e90dcf
Korey Enright <korey.enright@oracle.com>
2015-04-22 13:40:30 -0500

    Edited Gems for Windows Dev, and generated specs

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,8 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
 require "sprockets/railtie"
-# require "rails/test_unit/railtie"
+require "rails/test_unit/railtie"
+require "rspec-rails"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
