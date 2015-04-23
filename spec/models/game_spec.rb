d504f1804d66612ac437be33e69d520fa080712e
Korey Enright <korey.enright@oracle.com>
2015-04-23 10:12:51 -0500

    Added game validations and gitignore .bak

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Game, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    subject {FactoryGirl.build(:game)}
+    
+    it {should validate_presence_of(:title)}
+    it {should validate_uniqueness_of(:title)}
+  end
 end
