da434aae7e9450da6b5c54227a317b6203f17bc1
Red Lhota <rose.as.romeo@gmail.com>
2015-04-30 13:50:27 -0500

    Fix end route problem

--- config/routes.rb
+++ config/routes.rb
@@ -34,6 +34,7 @@ Rails.application.routes.draw do
       end
 
     end
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
