3c85202639aa28f23bb7bc70abf1c2540666a979
Red Lhota <rose.as.romeo@gmail.com>
2015-06-07 16:18:43 -0500

    Add status helper methods to game

--- app/models/game.rb
+++ app/models/game.rb
@@ -5,4 +5,41 @@ class Game < ActiveRecord::Base
   validates :title, :status, presence: true
   validates :title, uniqueness: { case_senstive: false }
   validates :status, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 2}
+
+  def has_started?
+    if status == 0
+      return false
+    else
+      return true
+    end
+  end
+
+  def start
+    self.status = 1
+  end
+
+  def mark_not_started
+    self.status = 0
+  end
+
+  def is_running?
+    if status == 1
+      return true
+    else
+      return false
+    end
+  end
+
+  def mark_complete
+    self.status = 2
+  end
+
+  def is_complete?
+    if status == 2
+      return true
+    else
+      return false
+    end
+  end
+
 end
